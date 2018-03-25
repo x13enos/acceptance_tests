06469488202af3dbf23ca549d19d65054d9539fb
Andres <x3enos@gmail.com>
2018-03-25 19:35:21 +0300

    Added env file for application

---
+++ features/support/env.rb
@@ -0,0 +1,10 @@
+require 'watir'
+require 'selenium-webdriver'
+require 'minitest/test'
+
+module MinitestAssertionsBridge
+  attr_accessor :assertions
+end
+
+World(MiniTest::Assertions)
+World(MinitestAssertionsBridge)
